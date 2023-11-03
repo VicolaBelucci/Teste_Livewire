17e4eef83c5ef01a5aafa9acc88d4e1c50ab798e
Bruno Belucci <brunobelucci97@gmail.com>
2023-11-03 10:46:18 -0300

    Model + Migration + Routes + Layout

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Livewire\PostCreate;
+use App\Livewire\PostEdit;
+use App\Livewire\PostIndex;
+use App\Livewire\PostShow;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +20,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/post/create', PostCreate::class)->name('post.create');
+Route::get('/post/edit/{post}', PostEdit::class)->name('post.edit');
+Route::get('/post/{post}', PostShow::class)->name('post.show');
+Route::get('/post', PostIndex::class)->name('post.index');
